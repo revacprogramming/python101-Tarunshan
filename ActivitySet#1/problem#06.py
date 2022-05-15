f23a4ad8dfbf4a1b662970d4f29b4548cf1e579b
Tarunshan <94883107+Tarunshan@users.noreply.github.com>
2022-05-15 17:34:11 +0000

    -

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,20 @@
-# Loops & Iterators
-
 largest = None
 smallest = None
-
 while True:
-    num = input("Enter a number? ")
-
+    num = input("Enter a number: ")
     if num == "done":
         break
-
-    # ...
-
     print(num)
-
+    num=int(num)
+    l=[]
+    l.append(num)
+    i=0
+    largest=l[i]
+    smallest=l[i]
+    for i in l:
+        if l[i]>largest:
+            largest=l[i]
+        if l[i]<smallest:
+            smallest=l[i]
 print("Maximum", largest)
+print("Minimum",smallest)
