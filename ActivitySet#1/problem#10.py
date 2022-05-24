2209eef5ef17ce75de953028687e29beadd67c18
Tarunshan <94883107+Tarunshan@users.noreply.github.com>
2022-05-24 10:17:39 +0000

    -

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,17 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+handle = open(name)
+d=dict()
+for line in handle:
+    if line.startswith("From:"):
+        l=line.split()
+        if l[1] not in d:
+            d[l[1]]=1
+        else:
+            d[l[1]]+=1
+l=0
+k=None
+for n,m in d.items():
+    if m>l:
+        l=m
+        k=n
+print(k,l)
