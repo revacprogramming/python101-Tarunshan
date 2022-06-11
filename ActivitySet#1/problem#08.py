478dd02a72b930e7f758b211607b41e672a67ef4
Tarunshan <94883107+Tarunshan@users.noreply.github.com>
2022-06-11 15:53:58 +0000

    -

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,6 +1,6 @@
 # Use the file name mbox-short.txt as the file name
-fname = input("Enter file name: ")
-fh = open(fname)
+fname=input("Enter file name: ")
+fh=open(fname)
 s=0
 count=0
 for line in fh:
